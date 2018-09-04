1174e2be350f29f121a9e134311df0b9722b050e
Mini Maker Lab <minimakerlab@gmail.com>
2018-09-04 14:04:54 -0300

    Add files via upload

---
+++ Desafios/Desafios.ino
@@ -0,0 +1,167 @@
+int ledPin=13,botao=2,speakerPin=11,interval=4000;
+unsigned long previousMillis[14];
+bool logState[14];
+#define c 261
+#define d 294
+#define e 329
+#define f 349
+#define g 391
+#define gS 415
+#define a 440
+#define aS 455
+#define b 466
+#define cH 523
+#define cSH 554
+#define dH 587
+#define dSH 622
+#define eH 659
+#define fH 698
+#define fSH 740
+#define gH 784
+#define gSH 830
+#define aH 880
+
+void setup(){  
+  pinMode(botao,INPUT_PULLUP);
+  for(int i=4;i<7;i++){
+      pinMode(i,OUTPUT);
+      previousMillis[i]=0;
+      logState[i]=true; 
+  }  
+  pinMode(ledPin, OUTPUT);
+  pinMode(speakerPin, OUTPUT);  
+}    
+     
+void loop(){
+  unsigned long currentMillis=millis(); 
+  if(digitalRead(botao)==LOW){
+     march();
+  }
+   if(analogRead(1)>=300){
+      digitalWrite(ledPin,HIGH);
+    }else{
+      digitalWrite(ledPin,LOW);
+    }
+    if(currentMillis-previousMillis[6]>=interval){
+        previousMillis[6] = currentMillis;
+        if (logState[6] == true){
+          digitalWrite(6, LOW);
+          digitalWrite(4, HIGH);
+          logState[6]=false;                    
+        }else if(logState[5] == true){
+          digitalWrite(4, LOW);
+          digitalWrite(5, HIGH);
+          logState[5]=false;
+          interval=2000;  
+        }else{
+          digitalWrite(6, HIGH);
+          digitalWrite(5, LOW);
+          logState[6]=true;
+          logState[5]=true;
+          interval=4000;  
+        }       
+    }
+}    
+     
+void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds){ 
+    int x;   
+    long delayAmount = (long)(1000000/frequencyInHertz);
+    long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
+    for (x=0;x<loopTime;x++){    
+        digitalWrite(speakerPin,HIGH);
+        delayMicroseconds(delayAmount);
+        digitalWrite(speakerPin,LOW);
+        delayMicroseconds(delayAmount);
+    }    
+    delay(20);
+}
+     
+void march(){    
+    beep(speakerPin, a, 500); 
+    beep(speakerPin, a, 500);     
+    beep(speakerPin, a, 500); 
+    beep(speakerPin, f, 350); 
+    beep(speakerPin, cH, 150);
+    
+    beep(speakerPin, a, 500);
+    beep(speakerPin, f, 350);
+    beep(speakerPin, cH, 150);
+    beep(speakerPin, a, 1000);
+    //first bit
+    
+    beep(speakerPin, eH, 500);
+    beep(speakerPin, eH, 500);
+    beep(speakerPin, eH, 500);    
+    beep(speakerPin, fH, 350); 
+    beep(speakerPin, cH, 150);
+    
+    beep(speakerPin, gS, 500);
+    beep(speakerPin, f, 350);
+    beep(speakerPin, cH, 150);
+    beep(speakerPin, a, 1000);
+    //second bit...
+    
+    beep(speakerPin, aH, 500);
+    beep(speakerPin, a, 350); 
+    beep(speakerPin, a, 150);
+    beep(speakerPin, aH, 500);
+    beep(speakerPin, gSH, 250); 
+    beep(speakerPin, gH, 250);
+    
+    beep(speakerPin, fSH, 125);
+    beep(speakerPin, fH, 125);    
+    beep(speakerPin, fSH, 250);
+    delay(250);
+    beep(speakerPin, aS, 250);    
+    beep(speakerPin, dSH, 500);  
+    beep(speakerPin, dH, 250);  
+    beep(speakerPin, cSH, 250);  
+    //start of the interesting bit
+    
+    beep(speakerPin, cH, 125);  
+    beep(speakerPin, b, 125);  
+    beep(speakerPin, cH, 250);      
+    delay(250);
+    beep(speakerPin, f, 125);  
+    beep(speakerPin, gS, 500);  
+    beep(speakerPin, f, 375);  
+    beep(speakerPin, a, 125); 
+    
+    beep(speakerPin, cH, 500); 
+    beep(speakerPin, a, 375);  
+    beep(speakerPin, cH, 125); 
+    beep(speakerPin, eH, 1000); 
+    //more interesting stuff (this doesn't quite get it right somehow)
+    
+    beep(speakerPin, aH, 500);
+    beep(speakerPin, a, 350); 
+    beep(speakerPin, a, 150);
+    beep(speakerPin, aH, 500);
+    beep(speakerPin, gSH, 250); 
+    beep(speakerPin, gH, 250);
+    
+    beep(speakerPin, fSH, 125);
+    beep(speakerPin, fH, 125);    
+    beep(speakerPin, fSH, 250);
+    delay(250);
+    beep(speakerPin, aS, 250);    
+    beep(speakerPin, dSH, 500);  
+    beep(speakerPin, dH, 250);  
+    beep(speakerPin, cSH, 250);  
+    //repeat... repeat
+    
+    beep(speakerPin, cH, 125);  
+    beep(speakerPin, b, 125);  
+    beep(speakerPin, cH, 250);      
+    delay(250);
+    beep(speakerPin, f, 250);  
+    beep(speakerPin, gS, 500);  
+    beep(speakerPin, f, 375);  
+    beep(speakerPin, cH, 125); 
+           
+    beep(speakerPin, a, 500);            
+    beep(speakerPin, f, 375);            
+    beep(speakerPin, c, 125);            
+    beep(speakerPin, a, 1000);       
+    //and we're done \ó/    
+}
